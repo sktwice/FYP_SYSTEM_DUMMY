a549536c21f5315ae98a35b6c46b715a5ddec86d
zahidi <167437083+sktwice@users.noreply.github.com>
2024-05-26 23:32:15 +0800

    Create Form1.java

---
+++ src/java/com/fyp/model/bean/Form1.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fyp.model.bean;
+
+/**
+ *
+ * @author User
+ */
+public class Form1 extends Form{
+    
+    private String termSv;
+    private String dateTSv;
+    private String termStu;
+    private String dateTStu;
+    private String agreementSv;
+    private String dateASv;
+    private String agreementStu;
+    private String dateAStu;
+    
+    public Form1(int formId, int studentId, int lId, int proId, String termSv, String dateTSv, String termStu, String dateTStu, String agreementSv, String dateASv, String agreementStu, String dateAStu) {
+        super(formId, studentId, lId, proId);
+        this.termSv=termSv;
+        this.dateTSv=dateTSv;
+        this.termStu=termStu;
+        this.dateTStu=dateTStu;
+        this.agreementSv=agreementSv;
+        this.dateASv=dateASv;
+        this.agreementStu=agreementStu;
+        this.dateAStu=dateAStu;
+    }
+
+    public String getTermSv() {
+        return termSv;
+    }
+
+    public String getDateTSv() {
+        return dateTSv;
+    }
+
+    public String getTermStu() {
+        return termStu;
+    }
+
+    public String getDateTStu() {
+        return dateTStu;
+    }
+
+    public String getAgreementSv() {
+        return agreementSv;
+    }
+
+    public String getDateASv() {
+        return dateASv;
+    }
+
+    public String getAgreementStu() {
+        return agreementStu;
+    }
+
+    public String getDateAStu() {
+        return dateAStu;
+    }
+
+    public void setTermSv(String termSv) {
+        this.termSv = termSv;
+    }
+
+    public void setDateTSv(String dateTSv) {
+        this.dateTSv = dateTSv;
+    }
+
+    public void setTermStu(String termStu) {
+        this.termStu = termStu;
+    }
+
+    public void setDateTStu(String dateTStu) {
+        this.dateTStu = dateTStu;
+    }
+
+    public void setAgreementSv(String agreementSv) {
+        this.agreementSv = agreementSv;
+    }
+
+    public void setDateASv(String dateASv) {
+        this.dateASv = dateASv;
+    }
+
+    public void setAgreementStu(String agreementStu) {
+        this.agreementStu = agreementStu;
+    }
+
+    public void setDateAStu(String dateAStu) {
+        this.dateAStu = dateAStu;
+    }
+    
+    
+    
+    
+}
